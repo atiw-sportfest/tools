d3c9ab08aceedb3ef10eb86e2b53c82db2a40bea
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-12-04 11:46:06 +0100

    Fix install

--- install/sporfest_setup.sh
+++ install/sporfest_setup.sh
@@ -3,13 +3,15 @@
 BASEDIR="/c/sportfest"
 MYPASS="s3cret"
 
+inittmp=$(mktemp)
+
 # Install
 
 ## TomEE
 
-[ -z "$TOMEE"] && TOMEE=$(ls -1 $(cygpath $USERPROFILE)/Downloads/apache-tomee*.zip | sort --sort=version | head -n1)
-[ -z "$TOMEE"] && TOMEE=$(ls -1 $(pwd)/apache-tomee*.zip | sort --sort=version | head -n1)
-[ -z "$TOMEE"] && curl -L "http://repo.maven.apache.org/maven2/org/apache/tomee/apache-tomee/7.0.4/apache-tomee-7.0.4-webprofile.zip" && \
+[ -z "$TOMEE" ] && TOMEE=$(ls -1 $(cygpath $USERPROFILE)/Downloads/apache-tomee*.zip | sort --sort=version | head -n1)
+[ -z "$TOMEE" ] && TOMEE=$(ls -1 $(pwd)/apache-tomee*.zip | sort --sort=version | head -n1)
+[ -z "$TOMEE" ] && curl -L {"http://repo.maven.apache.org/maven2/org/apache/tomee/apache-tomee/7.0.4/","-o "}apache-tomee-7.0.4-webprofile.zip && \
     TOMEE=$(ls -1 $(pwd)/apache-tomee*.zip | sort --sort=version | head -n1)
 
 while [ -z "$TOMEE" ] || [ ! -f "$TOMEE" ]; do
@@ -23,7 +25,7 @@ echo $TOMEE
 
 [ -z "$MARIADB" ] && MARIADB=$(ls -1 $(cygpath $USERPROFILE)/Downloads/mariadb*.zip | sort --sort=version | head -n1)
 [ -z "$MARIADB" ] && MARIADB=$(ls -1 $(pwd)/mariadb*.zip | sort --sort=version | head -n1)
-[ -z "$MARIADB" ] && curl -L "https://downloads.mariadb.org/interstitial/mariadb-10.2.11/win32-packages/mariadb-10.2.11-win32.zip" && \
+[ -z "$MARIADB" ] && curl -L {"https://downloads.mariadb.org/interstitial/mariadb-10.2.11/win32-packages/","-o "}mariadb-10.2.11-win32.zip && \
     MARIADB=$(ls -1 $(pwd)/mariadb*.zip | sort --sort=version | head -n1)
 
 while [ -z "$MARIADB" ] || [ ! -f "$MARIADB" ]; do
@@ -40,6 +42,7 @@ mkdir -p "${BASEDIR}"/{mariadb,tomee}
 ## Tomcat
 
 cd "${BASEDIR}"/tomee
+
 unzip $TOMEE && mv apache-tomee*/* . && rm -rf apache-tomee*
 
 ## MariaDB
@@ -50,13 +53,14 @@ unzip $MARIADB && mv mariadb*/* . && rm -rf mariadb*
 ## Database
 bin/mysql_install_db.exe --datadir db --password "${MYPASS}" # --password is windows only
 
-bin/mysqld --console &
-bin/mysqladmin create sportfest
-kill $!
+echo -e "CREATE DATABASE sportfest;\nSHUTDOWN;" > $inittmp
+
+bin/mysqld --init-file $inittmp --console
+rm $inittmp
 
 ## Connector
 
-curl {https://downloads.mariadb.com/Connectors/java/connector-java-2.2.0/,../tomee/lib/}mariadb-java-client-2.2.0.jar
+cd ../tomee/lib && curl -L {"https://downloads.mariadb.com/Connectors/java/connector-java-2.2.0/","-o "}mariadb-java-client-2.2.0.jar
  
 cat >/c/sportfest/sportfest_run.bat <<EOT
 cd /c $(cygpath -w "${BASEDIR}")
